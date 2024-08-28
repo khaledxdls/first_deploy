6b57dc735f1636643c6de1ae8203a5698ca32bd9
unknown <khaleddelassi03@gmail.com>
2024-08-28 12:02:41 +0100

    web socket

--- index_ws.js
+++ index_ws.js
@@ -6,27 +6,48 @@ const PORT = 3000;
 app.get("/", function (req, res) {
   res.sendFile("index.html", { root: __dirname });
 });
+
 server.on("request", app);
 
 server.listen(PORT, function () {
   console.log("Listening on " + PORT);
 });
 
-/* Web socket */
+/** Websocket **/
 const WebSocketServer = require("ws").Server;
+
 const wss = new WebSocketServer({ server: server });
-wss.on("connection", function (ws) {
-  nm_client = wss.clients.size;
-  console.log("Client connected", nm_client);
-  wss.broadcast(`current visitors: ${nm_client}`);
-  ws.readyState === ws.OPEN && ws.send("Welcome to the chat room");
-  ws.on("close", function (msg) {
-    console.log("Client disconnected", nm_client);
+
+wss.on("connection", function connection(ws) {
+  const numClients = wss.clients.size;
+
+  console.log("clients connected: ", numClients);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("welcome!");
+  }
+
+  ws.on("close", function close() {
+    wss.broadcast(`Current visitors: ${wss.clients.size}`);
+    console.log("A client has disconnected");
+  });
+
+  ws.on("error", function error() {
+    //
   });
 });
 
-wss.broadcast = function (data) {
-  wss.clients.forEach(function (client) {
+/**
+ * Broadcast data to all connected clients
+ * @param  {Object} data
+ * @void
+ */
+wss.broadcast = function broadcast(data) {
+  console.log("Broadcasting: ", data);
+  wss.clients.forEach(function each(client) {
     client.send(data);
   });
 };
+/** End Websocket **/
