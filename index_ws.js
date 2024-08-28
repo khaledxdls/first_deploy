d7253a3508cb07c2326a4f9bdd96f16d1513d34a
unknown <khaleddelassi03@gmail.com>
2024-08-28 13:59:28 +0100

    web socket

--- index_ws.js
+++ index_ws.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const server = require("http").createServer();
-const CryptoJS = require("crypto-js");
-const crypto = require("crypto");
 const app = express();
 const PORT = 3000;
 
